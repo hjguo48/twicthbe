1bc6fab4057dd66d1dbcc1c3073ba9c53c827466
hjguo48 <hjguo48@gmail.com>
2025-03-19 13:48:35 -0400

    exception controller

--- src/main/java/com/laioffer/twitch/AppConfig.java
+++ src/main/java/com/laioffer/twitch/AppConfig.java
@@ -4,7 +4,14 @@ package com.laioffer.twitch;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -20,4 +27,17 @@ public class AppConfig {
                 );
         return http.build();
     }
+
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
